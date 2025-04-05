23a0cbb8dcb3c78d1cf53fb72e295f896dee6b10
Jatin010700 <130746825+Jatin010700@users.noreply.github.com>
2025-04-05 17:53:37 +0400

    completed

--- my_portfolio/components/header.tsx
+++ my_portfolio/components/header.tsx
@@ -6,7 +6,7 @@ export function Header() {
     <>
     <div className="flex flex-col justify-center items-center pt-20 mx-5">
       <h1 className="mb-2 text-4xl font-bold tracking-tighter md:text-5xl lg:text-6xl">
-        Hi, I'm <AuroraText>Jatin Oomajee</AuroraText>
+        Hi, I&apos;m <AuroraText>Jatin Oomajee</AuroraText>
       </h1>
       <p className="md:w-2/4">
       Passionate Front-End/FullStack developer. I have independently pursued learning opportunities to enhance my capabilities in IT and development. 
