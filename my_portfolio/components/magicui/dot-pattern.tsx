d5851c0d70ac8d4c273c9b9b688eca182fb6cf13
Jatin010700 <130746825+Jatin010700@users.noreply.github.com>
2025-04-05 18:13:58 +0400

    fix prod error

--- my_portfolio/components/magicui/dot-pattern.tsx
+++ my_portfolio/components/magicui/dot-pattern.tsx
@@ -20,8 +20,6 @@ import React, { useEffect, useId, useRef, useState } from "react";
 interface DotPatternProps extends React.SVGProps<SVGSVGElement> {
   width?: number;
   height?: number;
-  x?: number;
-  y?: number;
   cx?: number;
   cy?: number;
   cr?: number;
@@ -63,8 +61,6 @@ interface DotPatternProps extends React.SVGProps<SVGSVGElement> {
 export function DotPattern({
   width = 16,
   height = 16,
-  x = 0,
-  y = 0,
   cx = 1,
   cy = 1,
   cr = 1,
@@ -98,14 +94,9 @@ export function DotPattern({
     (_, i) => {
       const col = i % Math.ceil(dimensions.width / width);
       const row = Math.floor(i / Math.ceil(dimensions.width / width));
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const x = col * width + cx;
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const y = row * height + cy;
-
       return {
-        x,
-        y,
+        x: col * width + cx,
+        y: row * height + cy,
         delay: Math.random() * 5,
         duration: Math.random() * 3 + 2,
       };
@@ -128,7 +119,7 @@ export function DotPattern({
           <stop offset="100%" stopColor="currentColor" stopOpacity="0" />
         </radialGradient>
       </defs>
-      {dots.map((dot /* eslint-disable-line @typescript-eslint/no-unused-vars */, index /* eslint-disable-line @typescript-eslint/no-unused-vars */) => (
+      {dots.map((dot) => (
         <motion.circle
           key={`${dot.x}-${dot.y}`}
           cx={dot.x}
